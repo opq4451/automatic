522dd53ab29696e43a88a100fb61cace80d400ad
許智富 <shezfuhsu@gamasys.com.tw>
2017-10-17 16:47:47 +0800

    過關紀錄 顯示期號跟名和關數就好

--- ap/src/main/java/com/uf/automatic/controller/Controller.java
+++ ap/src/main/java/com/uf/automatic/controller/Controller.java
@@ -522,8 +522,9 @@ public class Controller {
 				String key = phase  + "@" + sn + "@" + c[i] ;
 				if(configProperty.getProperty(key) != null){
 					over_i++;
-					Utils.WritePropertiesFile(user+"overLOGDIS_log", fillZero(Integer.toString(over_i)), "第"+phase + "期，第" + sn + "名，號碼(" + code + ") 已過關!(第"+c+"關)");
-				 
+					//Utils.WritePropertiesFile(user+"overLOGDIS_log", fillZero(Integer.toString(over_i)), "第"+phase + "期，第" + sn + "名，號碼(" + code + ") 已過關!(第"+c+"關)");
+					Utils.WritePropertiesFile(user+"overLOGDIS_log", fillZero(Integer.toString(over_i)), "第"+phase + "期，第" + sn + "名，已過關!(第"+c+"關)");
+
 					j.addProperty(covertIntToLatter(sn), "Y");
 
 				}	
